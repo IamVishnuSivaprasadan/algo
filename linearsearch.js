8f3da9b7ab97cd3476e7073575b9eb06fe8be9c3
IamVishnuSivaprasadan <vishnu.sivaprasadan@gmail.com>
2023-09-09 12:19:21 +0530

    ls

--- linearsearch.js
+++ linearsearch.js
@@ -1,1 +1,23 @@
-hello
+const linearSearch=(arr,target)=>{
+    for(i=0 ; i< arr.length ; i++){
+        if(arr[i]=== target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+const prompt = require('prompt-sync')();
+const inputArray = prompt("Enter list of numbers seprated by comma :");
+const targetValue = parseInt(prompt("Enter value to  be searched:"));
+
+const array = inputArray.split(',').map(Number);
+const result = linearSearch(array,targetValue);
+
+if (result !== -1) {
+    console.log('Value ' +targetValue+ ' found at index '+result+'.');
+}
+else{
+    console.log('Value ' +targetValue+' was not found');
+}
+
